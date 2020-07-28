37fa1c3295b9e665a9731c18fb514491aa039b27
slee713 <steven.lee1393@gmail.com>
2020-07-28 15:41:34 -0400

    create and start cli.rb

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,14 @@ require_relative '../config/environment'
 
 
 
-puts "HELLO WORLD"
+display
+greeting
+user_response = gets.chomp
+
+if user_response == 'Y'
+    puts "find account"
+elsif user_response == 'N'
+    puts "create account"
+end
+
+menu
