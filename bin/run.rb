964bc4e43b7b978279e0fb0348e600053ef1cade
slee713 <steven.lee1393@gmail.com>
2020-07-28 16:05:39 -0400

    added more functions

--- bin/run.rb
+++ bin/run.rb
@@ -5,11 +5,15 @@ require_relative '../config/environment'
 display
 greeting
 user_response = gets.chomp
+ask_for_username(user_response)
+# if user_response == 'Y'
+#     puts "Please enter username"
+#     user_input = gets.chomp
+#     user = User.find_by username: user_input
+#     puts "Welcome #{user.name}!"
+# elsif user_response == 'N'
+#     puts "create account"
+# end
 
-if user_response == 'Y'
-    puts "find account"
-elsif user_response == 'N'
-    puts "create account"
-end
+main_menu
 
-menu
--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -29,13 +29,46 @@ def greeting
     puts "Welcome to Travel Tracker! Do you have an account? (Y/N)"
 end
 
-def menu
+def ask_for_username(user_response)
+    if user_response == 'Y'
+        puts "Please enter username"
+        user_input = gets.chomp
+        user = User.find_by username: user_input
+        puts "Welcome #{user.name}!"
+    elsif user_response == 'N'
+        puts "create account"
+    end
+end
+
+def find_user
+end
+
+def create_account
+end
+
+
+
+def main_menu
     puts "1. Enter a new Trip"
     puts "2. Find all of you trips and update any trip" # list of trips we can 
     #have another menu with 1. View Stops for a trip 
     #2. Update trip. 
     #3. Delete Trip 
     puts "3. Find all of the states/countries you have visited"
+    puts "4. Delete Account"
+end
+
+def enter_new_trip
+end
+
+def find_trips
+end
+
+def find_all_states_and_countries
+end
+
+def delete_account
+end
 
 
 
