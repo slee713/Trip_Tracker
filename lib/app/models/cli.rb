ca2d62cccd6bd93b8fc12efc059810290925d636
slee713 <steven.lee1393@gmail.com>
2020-07-29 11:37:25 -0400

    finished find all states and countries

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -81,7 +81,7 @@ def main_menu(user)
     puts "3. Find all of the states/countries you have visited"
     puts "4. Delete Account"
     puts "5. Exit"
-    puts "Please select from the options above using numbers (1-4) as your input:"
+    puts "Please select from the options above using numbers (1-5) as your input:"
 
     user_input = gets.chomp
     
@@ -104,13 +104,13 @@ end
 def enter_new_trip(user)
     puts "Enter a new Trip"
     puts "What is the name of your trip?"
-    trip_name = gets.chomp
+        trip_name = gets.chomp
     puts "What was your method of transportation?"
-    transportation = gets.chomp
+        transportation = gets.chomp
     puts "What was your start date? (YYYY/MM/DD)"
-    start = gets.chomp
+        start = gets.chomp
     puts "What was your end date? (YYYY/MM/DD)"
-    user_end_date = gets.chomp
+        user_end_date = gets.chomp
     Trip.create(
         user_id: user.id, 
         name: trip_name, 
@@ -123,18 +123,18 @@ end
 def create_locations(new_trip)
     while true
         puts "Which city did you visit on this trip?"
-        location_name = gets.chomp.downcase
+            location_name = gets.chomp.downcase
         puts "Which state or country was it in?"
-        location_statecountry = gets.chomp.downcase
-        all_location_name = Location.all.map {|location| location.city_name.downcase}
+            location_statecountry = gets.chomp.downcase
+            all_location_name = Location.all.map {|location| location.city_name.downcase}
         if !all_location_name.include?(location_name.downcase)
             location = Location.create(
                 city_name: location_name,
                 state_or_country: location_statecountry
                 )
-            create_spots(new_trip)
-            puts "Did you visit another spot on this trip? (Y/N)"
-            response = gets.chomp.downcase
+            create_spots(new_trip, location)
+            # puts "Did you visit another spot on this trip? (Y/N)"
+            response = another_spot?
             if response == "N".downcase
                 break
             elsif response == "Y".downcase
@@ -143,8 +143,7 @@ def create_locations(new_trip)
         elsif all_location_name.include?(location_name.downcase)
             location = Location.find_by city_name: location_name 
             create_spots(new_trip, location)
-            puts "Did you visit another spot on this trip? (Y/N)"
-            response = gets.chomp.downcase
+            response = another_spot?
             if response == "N".downcase
                 break
             elsif response == "Y".downcase
@@ -154,7 +153,7 @@ def create_locations(new_trip)
     end
 end
 
-def create_spots(trip, location)
+def create_spots(trip, location) # doesnt work
     puts "How would you rate this stop? (1-10)?"
     user_rating = gets.chomp
     puts "Please write a description for this stop."
@@ -166,6 +165,12 @@ def create_spots(trip, location)
         description: user_description
     )
 end
+
+def another_spot?
+    puts "Did you visit another spot on this trip? (Y/N)"
+    gets.chomp.downcase
+end
+
 
 
 def find_trips(user)
@@ -180,14 +185,31 @@ def list_trips(trips)
 end
 
 def find_all_states_and_countries(user)
+    locations = []
+    trips = find_trips(user)
+    trips.each do |trip|
+        Stop.where(trip_id: trip.id).each do |stop|
+            locations << Location.find(stop.location_id)
+            
+        end
+    end
+    list_of_locations(locations.uniq)
 end
 
 def delete_account(user)
+    # puts "Please confirm that you would like to delete your account. (Y/N)"
+    #     user_input = gets.chomp.downcase
+    #     if user_input = 'y'
+    #         User.destroy(user.id)
+            
+    #         puts "Account Deleted"
+    #     end
 end
 
-def list_of_locations
-    Location.all.each_with_index {|location, index| 
-    puts "#{index+1}. #{location.name}, #{location.state_or_country}"
+def list_of_locations(location)
+    puts "Here are all of the locations you have visited!"
+    location.each_with_index {|location, index| 
+    puts "#{index+1}. #{location.city_name}, #{location.state_or_country}"
 }
 end
 
